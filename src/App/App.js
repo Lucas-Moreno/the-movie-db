19bb94d3139f0f6aeeed6268378fd0919f7006c0
Lucas-Moreno <moreno.lucas.82300@hotmail.fr>
2020-11-18 09:21:13 +0100

    first commit

---
+++ src/App/App.js
@@ -0,0 +1,13 @@
+import React from 'react';
+import '../css/style.css';
+import './App.css';
+
+const App = () => {
+  return (
+    <div className="App">
+      <h1 className="App__title">Nouvelle Application</h1>
+    </div>
+  );
+}
+
+export default App;
