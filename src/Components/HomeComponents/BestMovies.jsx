23ba064a4f68d9bba5c058c2afbae0addee4595b
Alexanne Brunold <alexanne.brunold@hetic.net>
2020-11-18 17:10:26 +0100

    Create function and display title and date

--- src/Components/HomeComponents/BestMovies.jsx
+++ src/Components/HomeComponents/BestMovies.jsx
@@ -4,32 +4,28 @@ import axios from 'axios';
 import { useState, useEffect } from 'react';
 
 const BestMovies = () => {
-  const [latestFilms, setPersons] = useState([]);
-
+  const [bestMovies, setPersons] = useState([]);
+  const API_KEY = process.env.REACT_APP_APIKEY;
   useEffect(() => {
     axios
-      .get(
-        `https://api.themoviedb.org/3/movie/latest?api_key=325e4a633b85a4ad0f68aa6594634b88&language=en-US`,
-      )
+      .get(`https://api.themoviedb.org/3/discover/movie?api_key=${API_KEY}`)
       .then((res) => {
-        console.log('data', res);
-        const latestFilmsA = res.data;
-        setPersons(latestFilmsA);
-        console.log('persons', latestFilmsA);
+        const bestMoviesResults = res.data.results;
+        setPersons(bestMoviesResults);
       })
       .catch((err) => console.log(err));
   }, []);
+  console.log('persons', bestMovies);
 
   return (
     <div className='bestmovies'>
-      <h1>Les meilleurs films</h1>
-      <p>Tout voir</p>
-      {/* <p>
-        {data.persons.map((person) => (
-          <li>{person.name}</li>
-        ))}
-      </p> */}
-      <p>{JSON.stringify(latestFilms)}</p>
+      <img src='' alt='' />
+      {bestMovies.map((bestMovie) => (
+        <ul className='bestmovies--flex'>
+          <li>{bestMovie.title}</li>
+          <li>{bestMovie.release_date}</li>
+        </ul>
+      ))}
     </div>
   );
 };
