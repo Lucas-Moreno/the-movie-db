eb197639e7ccf8e46bbafa4128d3966562ee66d8
njsen <njsen93@gmail.com>
2020-11-19 22:17:54 +0100

    added: links to view all movies

--- src/Components/HomePage.jsx
+++ src/Components/HomePage.jsx
@@ -3,6 +3,7 @@ import MostViewedMovies from './HomeComponents/MostViewedMovies';
 import MostViewedTvShows from './HomeComponents/MostViewedTvShows';
 import MostTrendedMovies from './HomeComponents/MostTrendedMovies';
 import SearchBar from './Search/Search';
+import { Link } from 'react-router-dom';
 import AllKinds from './HomeComponents/AllKinds';
 
 import '../scss/style.scss';
@@ -17,22 +18,22 @@ const Movie = () => {
         </span>
         <div className='home__sliderContainer'>
           <span>
-            <p className='home__sliderContainer--title'>Les meilleurs films</p>
-            <p className='home__sliderContainer--viewAll'>Tout voir</p>
+            <p className='home__sliderContainer--title'>Les films populaires</p>
+            <Link to="/movies/popular" className='home__sliderContainer--viewAll'>Tout voir</Link>
           </span>
           <MostTrendedMovies />
         </div>
         <div className='home__sliderContainer'>
           <span>
             <p className='home__sliderContainer--title'>Les films les mieux notés</p>
-            <p className='home__sliderContainer--viewAll'>Tout voir</p>
+            <Link to="/movies/top_rated" className='home__sliderContainer--viewAll'>Tout voir</Link>
           </span>
           <MostViewedMovies />
         </div>
         <div className='home__sliderContainer'>
           <span>
             <p className='home__sliderContainer--title'>Séries les mieux notées</p>
-            <p className='home__sliderContainer--viewAll'>Tout voir</p>
+            <Link to="/" className='home__sliderContainer--viewAll'>Tout voir</Link>
           </span>
           <MostViewedTvShows />
         </div>
