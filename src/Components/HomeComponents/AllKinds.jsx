097dc4b3983f625af22cfb2956b6f44b41af3b97
Lucas-Moreno <moreno.lucas.82300@hotmail.fr>
2020-11-19 23:00:38 +0100

    title carousel genre

--- src/Components/HomeComponents/AllKinds.jsx
+++ src/Components/HomeComponents/AllKinds.jsx
@@ -10,66 +10,69 @@ const AllKinds = () => {
   let id = 6
 
   return (
-    <div className="container__carousel">
-      <Slider dots slidesPerRow={id}>
-        <Link to="/movie/film/Action">
-          <CardGenre name="Action" />
-        </Link>
-        <Link to="/movie/film/Thriller">
-          <CardGenre name="Thriller" />
-        </Link>
-        <Link to="/movie/film/Romance">
-          <CardGenre name="Romance" />
-        </Link>
-        <Link to="/movie/film/Aventure">
-          <CardGenre name="Aventure" />
-        </Link>
-        <Link to="/movie/film/Animation">
-          <CardGenre name="Animation" />
-        </Link>
-        <Link to="/movie/film/Comedie">
-          <CardGenre name="Comedie" />
-        </Link>
-        <Link to="/movie/film/Crime">
-          <CardGenre name="Crime" />
-        </Link>
-        <Link to="/movie/film/Documentaire">
-          <CardGenre name="Documentaire" />
-        </Link>
-        <Link to="/movie/film/Drama">
-          <CardGenre name="Drama" />
-        </Link>
-        <Link to="/movie/film/Famille">
-          <CardGenre name="Famille" />
-        </Link>
-        <Link to="/movie/film/Fantaisie">
-          <CardGenre name="Fantaisie" />
-        </Link>
-        <Link to="/movie/film/Histoire">
-          <CardGenre name="Histoire" />
-        </Link>
-        <Link to="/movie/film/Musique">
-          <CardGenre name="Musique" />
-        </Link>
-        <Link to="/movie/film/Mystere">
-          <CardGenre name="Mystere" />
-        </Link>
-        <Link to="/movie/film/ScienceFiction">
-          <CardGenre name="ScienceFiction" />
-        </Link>
-        <Link to="/movie/film/TvMovie">
-          <CardGenre name="TvMovie" />
-        </Link>
-        <Link to="/movie/film/Horreur">
-          <CardGenre name="Horreur" />
-        </Link>
-        <Link to="/movie/film/Guerre">
-          <CardGenre name="Guerre" />
-        </Link>
-        <Link to="/movie/film/Western">
-          <CardGenre name="Western" />
-        </Link>
-      </Slider>
+    <div>
+      <p className="title_carousel">Tout les genres :</p>
+      <div className="container__carousel">
+        <Slider dots slidesPerRow={id}>
+          <Link to="/movie/film/Action">
+            <CardGenre name="Action" />
+          </Link>
+          <Link to="/movie/film/Thriller">
+            <CardGenre name="Thriller" />
+          </Link>
+          <Link to="/movie/film/Romance">
+            <CardGenre name="Romance" />
+          </Link>
+          <Link to="/movie/film/Aventure">
+            <CardGenre name="Aventure" />
+          </Link>
+          <Link to="/movie/film/Animation">
+            <CardGenre name="Animation" />
+          </Link>
+          <Link to="/movie/film/Comedie">
+            <CardGenre name="Comedie" />
+          </Link>
+          <Link to="/movie/film/Crime">
+            <CardGenre name="Crime" />
+          </Link>
+          <Link to="/movie/film/Documentaire">
+            <CardGenre name="Documentaire" />
+          </Link>
+          <Link to="/movie/film/Drama">
+            <CardGenre name="Drama" />
+          </Link>
+          <Link to="/movie/film/Famille">
+            <CardGenre name="Famille" />
+          </Link>
+          <Link to="/movie/film/Fantaisie">
+            <CardGenre name="Fantaisie" />
+          </Link>
+          <Link to="/movie/film/Histoire">
+            <CardGenre name="Histoire" />
+          </Link>
+          <Link to="/movie/film/Musique">
+            <CardGenre name="Musique" />
+          </Link>
+          <Link to="/movie/film/Mystere">
+            <CardGenre name="Mystere" />
+          </Link>
+          <Link to="/movie/film/ScienceFiction">
+            <CardGenre name="ScienceFiction" />
+          </Link>
+          <Link to="/movie/film/TvMovie">
+            <CardGenre name="TvMovie" />
+          </Link>
+          <Link to="/movie/film/Horreur">
+            <CardGenre name="Horreur" />
+          </Link>
+          <Link to="/movie/film/Guerre">
+            <CardGenre name="Guerre" />
+          </Link>
+          <Link to="/movie/film/Western">
+            <CardGenre name="Western" />
+          </Link>
+        </Slider>
+      </div>
     </div>
   );
 };
