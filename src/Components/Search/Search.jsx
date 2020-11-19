2a1ba378b2a25f42155144599f9f9a560effa975
Alexanne Brunold <alexanne.brunold@hetic.net>
2020-11-19 14:13:11 +0100

    Add component Search

---
+++ src/Components/Search/Search.jsx
@@ -0,0 +1,13 @@
+import React from 'react';
+
+import '../scss/style.scss';
+
+const SearchBar = () => {
+  return (
+    <div className='searchBar'>
+      <h1>Search Bar</h1>
+    </div>
+  );
+};
+
+export default SearchBar;
