149ae86d0e0259bb6ca4ca4ae99b401617bbd264
Alexanne Brunold <alexanne.brunold@hetic.net>
2020-11-19 17:15:38 +0100

    Style card

--- src/Components/Cards/CardSearch.jsx
+++ src/Components/Cards/CardSearch.jsx
@@ -3,14 +3,12 @@ import React from 'react';
 import '../../scss/style.scss';
 
 const CardSearch = ({ x }) => {
+  const img = `https://image.tmdb.org/t/p/w500`;
+
   return x.slice(0, 4).map((item) => (
     <div className='cardSearch'>
-      {/* <img src={src} alt='' /> */}
+      <img src={img + item.poster_path} alt='' />
       <h1>{item.title}</h1>
-      <div>
-        <p>cc</p>
-        <p>c</p>
-      </div>
     </div>
   ));
 };
