ccafaa299493d0a27b5df34ef94d091ffb07e586
njsen <njsen93@gmail.com>
2020-11-19 15:38:25 +0100

    added: popular & top rated movies pages

--- src/App/App.js
+++ src/App/App.js
@@ -4,6 +4,7 @@ import './App.scss';
 import HomePage from '../Components/HomePage.jsx';
 import MovieDetails from '../Components/MovieDetails/MovieDetails.jsx';
 import NotFound from '../Components/NotFound/NotFound.jsx';
+import Movies from '../Components/Movies/Movies';
 import { BrowserRouter, Route, Switch, Redirect } from 'react-router-dom';
 
 const App = () => {
@@ -13,6 +14,7 @@ const App = () => {
         <Redirect exact path='/' to='/movie'></Redirect>
         <Route exact path='/movie' component={HomePage}></Route>
         <Route path='/movie/:id' render={(routerProps) => MovieDetails(routerProps)}></Route>
+        <Route path='/movies/:filter' component={Movies}/>
         <Route component={NotFound}></Route>
       </Switch>
     </BrowserRouter>
